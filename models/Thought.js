a91262fc0415b751c463fea18aef0b169dd74a5b
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-03-17 19:51:28 -0400

    wrote script for thought model and reaction schema

---
+++ models/Thought.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const { Schema, Types } = mongoose;
+
+const reactionSchema = new Schema({
+    reactionsId: {
+        type:Schema.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required:true,
+        maxlength:280
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+});
+
+const thoughtSchema = new Schema({
+    thoughText: {
+        type: String,
+        required: true,
+        maxlength:280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => new Date(createdAtVal).toISOString()
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema]
+}, {
+    toJSON: {
+        getters: true,
+        virtuals: true
+    },
+    id: false
+ }); 
+ 
+ thoughtSchema.virtual('reationCount').get(function() {
+    return this.reactions.length;
+
+ });
+ const Thought = mongoose.model('Thought', thoughtSchema);
+
+ module.exports = Thought;
