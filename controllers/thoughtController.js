11402811d15c38e3aac61ed68eff82b1e613cc5c
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-03-18 18:38:04 -0400

    started Thought controller

---
+++ controllers/thoughtController.js
@@ -0,0 +1,38 @@
+const Thought = require('models/Thought.js');
+const User = require('models/User.js');
+
+const thoughtController = {
+    getAllThoughts: async (req, res) => {
+        try {
+            const thoughts = await Thought.find();
+            res.json(thoughts);
+            
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+    getThoughtById: async (req, res) => {
+        try {
+            const thought = await Thought.findById(req.params.id);
+            if (!thought) {
+                res.status(404).json({ message: 'No thought found with this id!' });
+                return;
+            }
+            res.json(thought);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json(err);
+        }
+
+    },
+    createThought: async (req, res) => {
+        try {
+            const thought = await Thought.create(req.body);
+            const user = await User.findByIdAndUpdate(req.body.userId, { $push: { thoughts: thought._id } }, { new: true });
+            if (!user) {
+                res.status(404).json({ message: ' User not found!' });
+            }
+        }
+    }
+}
