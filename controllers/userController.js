ff43cc7e8534d6a01bc1518515c450abde4c8a86
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-03-19 22:46:59 -0400

    more work on the user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,5 +24,57 @@ const userController = {
             res.status(500).json(err);
         }
     },
-    
+    createUser: async (req, res) => {
+        try {
+            const user = await User.create(req.body);
+            res.status(200).json(user);
+        } catch (err) {
+            console.error(err);
+            res.status(400).json(err);
+        }
+    },
+    updateUser: async (req, res) => {
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true});
+            if (!user) {
+                res.status(404).json({ message: 'No user found with this id!'});
+                return;
+            }
+            res.json(user);
+        } catch (err) {
+            console.error(err);
+            res.status(400).json(err);
+        }
+    },
+    deleteUser: async (req, res) => {
+        try {
+            const user = await User.findByIdAndDelete(req.params.id);
+            if (!user) {
+                res.status(404).json({ message: 'No user found with this id!'});
+                return;
+            }
+            // Delete all thoughts associated with the user
+            await Thought.deleteMany({ _id: { $in: user.thoughts }});
+            res.json({ message: 'User and associated thoughts deleted!'});
+        } catch (err) {
+            console.error(err);
+            res.status(400).json(err);
+        }
+    },
+    addFriend: async (req, res) => {
+        try {
+            const user =- await User.findByIdAndUpdate(req.params.userId);
+            if (!user) {
+                res.status(404).json({ message: 'No user found with this id!'});
+                return;
+            }
+            user.friends.push(req.params.friendId);
+            await user.save();
+            res.json(user);
+        } catch (err) {
+            console.error(err);
+            res.status(400).json(err);
+
+        }
+    }
 }
