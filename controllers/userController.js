e4b57ed2820760e83e97422b4139492df9dbbb14
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-03-19 23:05:44 -0400

    fixed bugs

--- controllers/userController.js
+++ controllers/userController.js
@@ -63,7 +63,7 @@ const userController = {
     },
     addFriend: async (req, res) => {
         try {
-            const user =- await User.findByIdAndUpdate(req.params.userId);
+            const user = await User.findById(req.params.userId);
             if (!user) {
                 res.status(404).json({ message: 'No user found with this id!'});
                 return;
