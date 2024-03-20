af19a1306dbf13cc4bd2bf0fb420c8ca162aa071
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-03-19 22:56:16 -0400

    finished writing user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -74,7 +74,23 @@ const userController = {
         } catch (err) {
             console.error(err);
             res.status(400).json(err);
-
         }
+    },
+    removeFriend: async (req, res) => {
+        try {
+            const user = await User.findById(req.params.userId);
+            if (!user) {
+                res.status(404).json({ message: 'No user found with this id!'});
+                return;
+            }
+            user.friends.pull(req.params.friendId);
+            await user.save();
+        } catch (err) {
+            console.error(err);
+            res.status(400).json(err);
+        }
+
     }
-}
+};
+
+module.exports = userController;
